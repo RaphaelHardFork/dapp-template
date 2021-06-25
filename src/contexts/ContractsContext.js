eb9146fd58f5e0d83753f82aad4410fd4c96d539
RaphaelHardFork <raphael.pellet@protonmail.com>
2021-06-25 10:26:08 +0200

    cc

--- src/contexts/ContractsContext.js
+++ src/contexts/ContractsContext.js
@@ -1,6 +1,6 @@
 import { createContext } from "react"
 import { useContract } from "web3-hooks"
-import { contractAddress, contractABI } from "../components/contracts/contract"
+import { contractAddress, contractABI } from "../contracts/contract"
 
 export const ContractsContext = createContext(null)
 
